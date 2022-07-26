99095ab08cd7b3f08fa8ca233d2f56da72070b63
MMB MUSA <101189454+musa327@users.noreply.github.com>
2022-07-26 15:45:35 +0600

    Add files via upload

---
+++ Lab - 01/01. 327.(49-8).cpp
@@ -0,0 +1,23 @@
+// CODEFORCE
+//Problem No : 4A
+//Problem NaME : WATERMELON
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n>2 && n%2==0){
+        cout<<"Yes"<<endl;
+    }else{
+        cout<<"No"<<endl;
+    }
+
+    return 0;
+
+}
+
+
+
+
