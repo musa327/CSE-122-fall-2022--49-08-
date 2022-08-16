1b39d18396f0ac8a7631d8e5ad2606c8e4cc2503
MMB MUSA <101189454+musa327@users.noreply.github.com>
2022-08-16 16:40:55 +0600

    Add files via upload

---
+++ lab-5/2.cpp
@@ -0,0 +1,20 @@
+// CODEFORCE -
+//Problem No :266A
+//Problem NaME : A. Stones on the Table
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, count = 0;
+    cin >> n;
+    string s;
+    cin >> s;
+    for(int i=0; i<n; i++){
+        if(s[i] == s[i+1]){
+            count++;
+        }
+    }
+    cout << count << endl;
+}
