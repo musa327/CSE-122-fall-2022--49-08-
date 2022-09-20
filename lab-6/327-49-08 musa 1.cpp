714b9552fe05a1897112c23dfa7fe51b35b90127
MMB MUSA <101189454+musa327@users.noreply.github.com>
2022-09-20 15:29:19 +0600

    Add files via upload

---
+++ lab-6/327-49-08 musa 1.cpp
@@ -0,0 +1,16 @@
+// CODEFORCE -
+//Problem No :671A
+//Problem NaME : Elephant
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    if(x%5 == 0){
+        cout << x / 5;
+    }else{
+        cout << (x / 5) + 1;
+    }
+}
